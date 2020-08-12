0398a19dccc7ef5c89a6a9eded76a49e64db1eee
iminfinity <this.is.me.infinity@gmail.com>
2020-08-12 10:26:35 +0530

    form-input update

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,7 +5,6 @@ import "./form-input.styles.scss";
 const FormInput = ({ onChange, label, ...otherProps }) => (
   <div className="group">
     <input className="form-input" onChange={onChange} {...otherProps} />
-    {label ? <label className="form-input-label">{label}</label> : null}
   </div>
 );
 
